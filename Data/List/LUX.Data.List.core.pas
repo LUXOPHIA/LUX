4454f68a6148b67f0b54a7a299af5311169608df
luxidea <contact@luxidea.net>
2021-03-18 15:18:17 +0900

    TListObject / TListParent / TListChildr / TListParentEnumerator クラスを LUX.Data.List.core ユニットへ分離。

--- Data/List/LUX.Data.List.core.pas
+++ Data/List/LUX.Data.List.core.pas
@@ -1,4 +1,4 @@
-﻿unit LUX.Data.List;
+﻿unit LUX.Data.List.core;
 
 interface //#################################################################### ■
 
@@ -10,11 +10,6 @@ type //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
        TListParent         = class;
        TListChildr         = class;
      TListParentEnumerator = class;
-
-     TListParent<TChildr_:class> = class;
-     TListParentEnumerator<TChildr_:class> = class;
-
-     TNodeProc<_TNode_:class> = reference to procedure( const Node_:_TNode_ );
 
      //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【レコード】
 
@@ -138,50 +133,6 @@ type //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
        property Current :TListChildr read GetCurrent;
        ///// メソッド
        function MoveNext :Boolean;
-     end;
-
-     //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TListParent<TChildr_>
-
-     TListParent<TChildr_:class> = class( TListParent )
-     private
-     protected
-       ///// アクセス
-       function GetHeader :TChildr_;
-       function GetTailer :TChildr_;
-       function GetChildrs( const I_:Integer ) :TChildr_;
-       procedure SetChildrs( const I_:Integer; const Childr_:TChildr_ );
-     public
-       ///// プロパティ
-       property Header                      :TChildr_ read GetHeader                  ;
-       property Tailer                      :TChildr_ read GetTailer                  ;
-       property Childrs[ const I_:Integer ] :TChildr_ read GetChildrs write SetChildrs; default;
-       ///// メソッド
-       function GetEnumerator: TListParentEnumerator<TChildr_>;
-     end;
-
-     //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TListChildr<TParent_>
-
-     TListChildr<TParent_:class> = class( TListChildr )
-     private
-     protected
-       ///// アクセス
-       function GetParent :TParent_;
-       procedure SetParent( const Parent_:TParent_ );
-     public
-       ///// プロパティ
-       property Parent :TParent_ read GetParent write SetParent;
-     end;
-
-     //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TListParentEnumerator<TChildr_>
-
-     TListParentEnumerator<TChildr_:class> = class( TListParentEnumerator )
-     private
-     protected
-       ///// アクセス
-       function GetCurrent: TChildr_;
-     public
-       ///// プロパティ
-       property Current :TChildr_ read GetCurrent;
      end;
 
 //const //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【定数】
@@ -650,80 +601,6 @@ begin
 
      Result := _Current <> _Container.Origin;
 end;
-
-//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TListParent<TChildr_>
-
-//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& private
-
-//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& protected
-
-/////////////////////////////////////////////////////////////////////// アクセス
-
-function TListParent<TChildr_>.GetHeader :TChildr_;
-begin
-     Result := TChildr_( inherited Header );
-end;
-
-function TListParent<TChildr_>.GetTailer :TChildr_;
-begin
-     Result := TChildr_( inherited Tailer );
-end;
-
-//------------------------------------------------------------------------------
-
-function TListParent<TChildr_>.GetChildrs( const I_:Integer ) :TChildr_;
-begin
-     Result := TChildr_( inherited Childrs[ I_ ] );
-end;
-
-procedure TListParent<TChildr_>.SetChildrs( const I_:Integer; const Childr_:TChildr_ );
-begin
-     inherited Childrs[ I_ ] := TListChildr( Childr_ );
-end;
-
-//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& public
-
-/////////////////////////////////////////////////////////////////////// メソッド
-
-function TListParent<TChildr_>.GetEnumerator: TListParentEnumerator<TChildr_>;
-begin
-     Result := TListParentEnumerator<TChildr_>.Create( Self );
-end;
-
-//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TListChildr<TParent_>
-
-//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& private
-
-//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& protected
-
-/////////////////////////////////////////////////////////////////////// アクセス
-
-function TListChildr<TParent_>.GetParent :TParent_;
-begin
-     Result := TParent_( inherited Parent );
-end;
-
-procedure TListChildr<TParent_>.SetParent( const Parent_:TParent_ );
-begin
-     inherited Parent := TListParent( Parent_ );
-end;
-
-//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& public
-
-//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TListParentEnumerator<TChildr_>
-
-//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& private
-
-//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& protected
-
-/////////////////////////////////////////////////////////////////////// アクセス
-
-function TListParentEnumerator<TChildr_>.GetCurrent: TChildr_;
-begin
-     Result := TChildr_( inherited Current );
-end;
-
-//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& public
 
 //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【ルーチン】
 
