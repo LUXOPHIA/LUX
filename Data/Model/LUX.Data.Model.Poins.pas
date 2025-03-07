68ce2f1da30824edf84b311e7ea26df0a2953daa
luxidea <contact@luxidea.net>
2025-03-08 02:22:58 +0900

    LUX.Data.Model ラブラリを追加。

---
+++ Data/Model/LUX.Data.Model.Poins.pas
@@ -0,0 +1,80 @@
+﻿unit LUX.Data.Model.Poins;
+
+interface //#################################################################### ■
+
+uses LUX,
+     LUX.Data.List;
+
+type //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【 T Y P E 】
+
+     TPoin<TPos_;TPoinSet_:class> = class;
+     TPoinSet<TPos_;TPoin_:class> = class;
+
+     //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【 R E C O R D 】
+
+     //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【 C L A S S 】
+
+     //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TPoin<TPos_,TPoinSet_>
+
+     TPoin<TPos_;TPoinSet_:class> = class( TListChildr<TPoinSet_> )
+     private
+     protected
+       ///// A C C E S S O R
+       function GetPos :TPos_; virtual; abstract;
+       procedure SetPos( const Pos_:TPos_ ); virtual; abstract;
+     public
+       constructor Create( const Pos_:TPos_ ); overload;
+       constructor Create( const Pos_:TPos_; const Parent_:TPoinSet_ ); overload;
+       ///// P R O P E R T Y
+       property Pos :TPos_ read GetPos write SetPos;
+     end;
+
+     //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TPoinSet<TPos_,TPoin_>
+
+     TPoinSet<TPos_;TPoin_:class> = class( TListParent<TPoin_> )
+     private
+     protected
+     public
+     end;
+
+//$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【 R O U T I N E 】
+
+implementation //############################################################### ■
+
+//$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【 R E C O R D 】
+
+//$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【 C L A S S 】
+
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TPoin<TPos_,TPoinSet_>
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& private
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& protected
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& public
+
+constructor TPoin<TPos_,TPoinSet_>.Create( const Pos_:TPos_ );
+begin
+     inherited Create;
+
+     Pos := Pos_;
+end;
+
+constructor TPoin<TPos_,TPoinSet_>.Create( const Pos_:TPos_; const Parent_:TPoinSet_ );
+begin
+     inherited Create( Parent_ );
+
+     Pos := Pos_;
+end;
+
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TPoinSet<TPos_,TPoin_>
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& private
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& protected
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& public
+
+//$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【 R O U T I N E 】
+
+end. //######################################################################### ■
