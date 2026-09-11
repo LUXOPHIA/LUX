7e819cc6ce90116d17d444fdda1135a75ffd526b
luxidea <contact@luxidea.net>
2026-09-12 03:40:55 +0900

    LUX.Complex.FMX.D3. 着色用の絶対値圧縮を直接除算に簡略化。

--- Complex/FMX/LUX.Complex.FMX.D3.pas
+++ Complex/FMX/LUX.Complex.FMX.D3.pas
@@ -220,7 +220,7 @@ begin
 
                C := Func( C ).o;
 
-               C.Abso := C.Abso / ( _Scale + C.Abso );
+               C := C / ( _Scale + C.Abso );
 
                T2.X := 0.5 + C.R / 2;
                T2.Y := 0.5 + C.I / 2;
